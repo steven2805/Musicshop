f6903bae6d9023cc30899f9d535bdc6a81a3ef18
steven2805 <stevenmac90@googlemail.com>
2017-07-01 12:19:25 +0100

    customer cna have instrument

--- music_shop/Customer.java
+++ music_shop/Customer.java
@@ -1,15 +1,22 @@
 package music_shop;
+import behaviours.*;
 
 
 public class Customer{
   String name;
+  Playable instrument;
 
-  public Customer(String name){
+  public Customer(String name, Playable instrument){
     this.name = name;
+    this.instrument = instrument;
   }
 
   public String getName(){
     return this.name;
+  }
+
+  public Playable hasInstrument(){
+    return this.instrument;
   }
 
 
--- music_shop_test/CustomerTest.java
+++ music_shop_test/CustomerTest.java
@@ -4,14 +4,29 @@ import music_shop.*;
 
 public class CustomerTest {
   Customer customer ;
+  Guitar guitar;
 
   @Before 
   public void before(){
-    customer = new Customer("sam");
+    guitar = new Guitar("wood",100,5);
+    customer = new Customer("sam", guitar);
+
   }
 
   @Test 
   public void hasName(){
     assertEquals("sam", customer.getName());
   }
+
+  @Test 
+  public void hasInstrument(){
+    assertNotNull(customer.hasInstrument());
+  }
+
+
+
+
+
+
+
 }
